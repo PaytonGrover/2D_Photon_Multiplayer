03d98a3265bff6db5353d7beae60566bd9e600c9
PaytonGrover <46699050+PaytonGrover@users.noreply.github.com>
2020-11-02 19:39:48 -0700

    Initial commit

---
+++ Assets/Scripts/PhotonManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using Photon;
+using Photon.Chat;
+
+public class PhotonManager : MonoBehaviourPunCallbacks
+{
+    [Tooltip("The prefab to use for representing the player")]
+    public GameObject playerPrefab1;
+    public GameObject playerPrefab2;
+    public GameObject playerPrefab3;
+    public GameObject playerPrefab4;
+
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+               
+    }
+
+    
+    void Update()
+    {
+        
+    }
+    public override void OnConnectedToMaster()
+    {
+        PhotonNetwork.JoinLobby();
+    }
+    public override void OnJoinedLobby()
+    {
+        
+        PhotonNetwork.JoinOrCreateRoom("Room", new RoomOptions { MaxPlayers = 4 }, TypedLobby.Default);
+    }
+    public override void OnJoinedRoom()
+    {
+        if(PhotonNetwork.LocalPlayer.ActorNumber == 1)
+        {
+            PhotonNetwork.Instantiate(this.playerPrefab2.name, new Vector3(0f, 5f, 0f), Quaternion.identity, 0);
+        }
+        if (PhotonNetwork.LocalPlayer.ActorNumber == 2)
+        {
+            PhotonNetwork.Instantiate(this.playerPrefab3.name, new Vector3(0f, 5f, 0f), Quaternion.identity, 0);
+        }
+        if (PhotonNetwork.LocalPlayer.ActorNumber == 3)
+        {
+            PhotonNetwork.Instantiate(this.playerPrefab3.name, new Vector3(0f, 5f, 0f), Quaternion.identity, 0);
+        }
+        if (PhotonNetwork.LocalPlayer.ActorNumber == 4)
+        {
+            PhotonNetwork.Instantiate(this.playerPrefab4.name, new Vector3(0f, 5f, 0f), Quaternion.identity, 0);
+        }
+        //PhotonNetwork.Instantiate(this.playerPrefab1.name, new Vector3(0f, 5f, 0f), Quaternion.identity, 0);
+        //PhotonNetwork.Instantiate("Player1", transform.position, Quaternion.identity);
+    }
+}

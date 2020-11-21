215316de15d32189cf9864221174709db62e738c
starfaerie <54830622+starfaerie@users.noreply.github.com>
2020-11-21 13:52:24 -0700

    Added photonView.isMine for camera and controls

--- Assets/PlayerManagement.cs
+++ Assets/PlayerManagement.cs
@@ -8,10 +8,26 @@ public class PlayerManagement : MonoBehaviour
     public MonoBehaviour[] scriptsToIgnore;
 
     private PhotonView photonView;
+
+    private Photon.Pun.Demo.PunBasics.CameraWork cameraWork;
     // Start is called before the first frame update
     void Start()
     {
         photonView = GetComponent<PhotonView>();
+        cameraWork = this.gameObject.GetComponent<Photon.Pun.Demo.PunBasics.CameraWork>();
+
+
+        if (cameraWork != null)
+        {
+            if (photonView.IsMine)
+            {
+                cameraWork.OnStartFollowing();
+            }
+        }
+        else
+        {
+            Debug.LogError("<Color=Red><a>Missing</a></Color> CameraWork Component on playerPrefab.", this);
+        }
         if (!photonView.IsMine)
         {
             foreach (var script in scriptsToIgnore)
@@ -19,12 +35,6 @@ public class PlayerManagement : MonoBehaviour
                 script.enabled = false;
             }
         }
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
 
     }
 }

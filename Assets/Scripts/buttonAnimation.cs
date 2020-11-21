a562cb074338db597ac8269544c593514a4e6289
Bighennn <46721786+Bighennn@users.noreply.github.com>
2020-11-21 13:53:04 -0700

    Animating Doors
    worked on animating on trigger

---
+++ Assets/Scripts/buttonAnimation.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class buttonAnimation : MonoBehaviour
+{
+    [SerializeField]
+    GameObject door;
+    void OnTriggerEnter(Collider col)
+    {
+        door.transform.position += new Vector3(0, 4, 0);
+    }
+}
---
+++ Assets/Scripts/doorOpener.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class doorOpener : MonoBehaviour
+{
+    [SerializeField] private Animator myDoor = null;
+    [SerializeField] private bool openTrigger = false;
+
+    [SerializeField] private string doorOpen = "doorOpen";
+
+    private void OnTrigggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            if (openTrigger)
+            {
+                myDoor.Play("doorOpen", 0, 0.0f);
+                gameObject.SetActive(false);
+            }
+            else if (openTrigger)
+            {
+                //myDoor.Play("doorOpen", 0, 0.0f);
+                gameObject.SetActive(false);
+            }
+        }
+    }
+}
